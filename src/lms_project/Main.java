806f936aae8119a2c8f3f6fcc20889633a5e63cb
Sagar Niroula (Alt.) <antihacks36@gmail.com>
2025-08-24 21:24:59 +0545

    Delete src/lms_project directory

--- src/lms_project/Main.java
+++
@@ -1,9 +0,0 @@
-import com.view.LibraryDashboard;
-import com.view.RegisterMemberForm;
-import com.view.ViewBooksForm;
-
-public class Main {
-    public static void main(String[] args) {
-        new RegisterMemberForm();
-    }
-}
